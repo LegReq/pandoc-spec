d8c24b4be47335fbcaae7581dbb7f25fee18cab9
Kevin Dean <Kevin.Dean@datadevelopment.com>
2025-09-18 12:01:11 -0400

    Fixed copyright notice and spacing.

--- src/file.ts
+++ src/file.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import * as fs from "node:fs";
 import { globIterateSync } from "glob";
 import * as path from "node:path";
